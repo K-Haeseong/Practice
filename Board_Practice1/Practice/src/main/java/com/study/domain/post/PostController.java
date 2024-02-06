917c9fd8d968a54ad2c84a59bbc33365c88330ae
Aiden <kim_aiden@naver.com>
2024-02-06 17:02:23 +0900

    feat : 게시판 리스트 및 상세 조회 기능 추가

--- Board_Practice1/Practice/src/main/java/com/study/domain/post/PostController.java
+++ Board_Practice1/Practice/src/main/java/com/study/domain/post/PostController.java
@@ -5,6 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.swing.*;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -13,7 +17,11 @@ public class PostController {
     private final PostService postService;
 
     @GetMapping("/post/write")
-    public String openPostWrite(Model model) {
+    public String openPostWrite(@RequestParam(value = "id", required = false) Long id, Model model) {
+        if(id != null) {
+            PostResponse params = postService.findPostById(id);
+            model.addAttribute("params", params);
+        }
         return "post/write";
     }
 
@@ -21,6 +29,20 @@ public class PostController {
     public String savePost(PostRequest params) {
         postService.savePost(params);
         return "redirect:/post/list";
+    }
+
+    @GetMapping("/post/list")
+    public String openPostList(Model model) {
+        List<PostResponse> posts = postService.findAllPost();
+        model.addAttribute("posts", posts);
+        return "post/list";
+    }
+
+    @GetMapping("/post/view")
+    public String openPostView(Long id, Model model) {
+        PostResponse post = postService.findPostById(id);
+        model.addAttribute("post", post);
+        return "post/view";
     }
 
 }
