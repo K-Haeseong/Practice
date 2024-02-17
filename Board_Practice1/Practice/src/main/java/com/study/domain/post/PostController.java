cb5dea773d71417151b23a143f6991646dec8fc5
Aiden <kim_aiden@naver.com>
2024-02-17 23:59:19 +0900

    feat : 이전 페이지 정보 유지하기 - 게시글 상세조회/수정/삭제 시 이전 페이지 정보 유지

--- Board_Practice1/Practice/src/main/java/com/study/domain/post/PostController.java
+++ Board_Practice1/Practice/src/main/java/com/study/domain/post/PostController.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 @Controller
 @RequiredArgsConstructor
@@ -32,32 +35,49 @@ public class PostController {
     }
 
     @PostMapping("/post/update")
-    public String updatePost(PostRequest params, Model model) {
+    public String updatePost( @ModelAttribute("queryParams") SearchDto queryParams,
+                              @ModelAttribute("params") PostRequest params,
+                              Model model) {
         postService.updatePost(params);
-        Long id = params.getId();
-        MessageDto message = new MessageDto("게시글이 수정 되었습니다.", "/post/view" +"?id=" + id, RequestMethod.GET, null);
+        MessageDto message = new MessageDto("게시글이 수정 되었습니다.", "/post/list", RequestMethod.GET, queryParamsToMap(queryParams));
         return showMessageAndRedirect(message, model);
     }
 
     @GetMapping("/post/list")
-    public String openPostList(@ModelAttribute("params") SearchDto params, Model model) {
+    public String openPostList(@ModelAttribute("params") SearchDto params,
+                               Model model) {
         PagingResponse<PostResponse> response = postService.findAllPost(params);
         model.addAttribute("response", response);
         return "post/list";
     }
 
     @GetMapping("/post/view")
-    public String openPostView(Long id, Model model) {
+    public String openPostView( @ModelAttribute("params") SearchDto params,
+                                @RequestParam("id") Long id,
+                                Model model) {
         PostResponse post = postService.findPostById(id);
         model.addAttribute("post", post);
         return "post/view";
     }
 
     @PostMapping("/post/delete")
-    public String deletePost(Long id, Model model) {
+    public String deletePost( @ModelAttribute("queryParams") SearchDto queryParams,
+                              @RequestParam("id") Long id,
+                              Model model) {
         postService.deletePostById(id);
-        MessageDto message = new MessageDto("게시글이 삭제 되었습니다.", "/post/list", RequestMethod.GET, null);
+        MessageDto message = new MessageDto("게시글이 삭제 되었습니다.", "/post/list", RequestMethod.GET, queryParamsToMap(queryParams));
         return showMessageAndRedirect(message, model);
+    }
+
+    private Map<String, Object> queryParamsToMap(SearchDto queryParams) {
+        Map<String, Object> data  = new HashMap<>();
+        data.put("page", queryParams.getPage());
+        data.put("recordSize", queryParams.getRecordSize());
+        data.put("pageSize", queryParams.getPageSize());
+        data.put("keyword", queryParams.getKeyword());
+        data.put("searchType", queryParams.getSearchType());
+
+        return data;
     }
 
     // 사용자에게 메시지 전달 및 페이지 리다이렉트
